72f5da86a239a41b1511fa0fc2f49aa3dac39580
JoyceKi <jocelyne.kim18@gmail.com>
2023-11-29 10:44:50 +0100

    résolution conflits merge

--- assets/app.js
+++ assets/app.js
@@ -6,6 +6,6 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.scss';
+import '../assets/styles/app.scss';
 
-import './bootstrap';
+import '../node_modules/bootstrap/scss/bootstrap.scss';
