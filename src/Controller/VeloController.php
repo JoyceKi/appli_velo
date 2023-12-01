719b6156e2425722cf2ea52c4d1cf9978c3cb8e2
JoyceKi <jocelyne.kim18@gmail.com>
2023-12-01 10:44:28 +0100

    liens form déclarer vélo et confirmation et retour

--- src/Controller/VeloController.php
+++ src/Controller/VeloController.php
@@ -11,34 +11,37 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\Loader\Configurator\form;
 
 class VeloController extends AbstractController
 {
-         #[Route('/velo', name: 'app_velo')]
+        #[Route('/velo', name: 'app_velo')]
         // Récupérer les données du formulaire
         public function addForm(EntityManagerInterface $entityManager, Request $request)
         {
-        // création du form
-        $velo = new Velo();
-        $form = $this->createForm(InscriptionVeloType::class, $velo);
-        // recuppérer les données de la requête
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-        $velo = $form->getData();
-        // dd($data);
-        $entityManager->persist($velo);
-        $entityManager->flush();
-        return $this->redirectToRoute('app_velo');
+                // création du form
+                $velo = new Velo();
+                $form = $this->createForm(InscriptionVeloType::class, $velo);
+                // recuppérer les données de la requête
+                $form->handleRequest($request);
+                if ($form->isSubmitted() && $form->isValid()) {
+                        $velo = $form->getData();
+                        // dd($data);
+                        $entityManager->persist($velo);
+                        $entityManager->flush();
+                        return $this->redirectToRoute('app_velo_confirmation');
+                }
+                return $this->render('velo/index.html.twig', [
+                        // afficher le formulaire avec la variable twig
+                        'formulaire' => $form->createView(),
+                         // Rediriger vers la route de confirmation
+                ]);
         }
-        return $this->render('velo/add.html.twig', [
-        'controller_name' => 'VeloController',
-        // afficher le formulaire avec la variable twig
-        'formulaire' => $form->createView(),
-        ]);
+
+        #[Route('/velo/confirmation', name: 'app_velo_confirmation')]
+        public function confirmation(): Response
+        {
+            return $this->render('velo/confirmation.html.twig');
         }
         
-
-      
-
 }
-
